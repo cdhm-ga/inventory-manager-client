cbb91e2020dec058a184e10fe3174f731ea24319
matthewjmontalto <matthewjmontalto@gmail.com>
2019-04-01 10:24:11 -0400

    Style inventory table buttons and user feedback
    - apply flex behavior to table buttons and user feedback div
    - wrap buttons and feedback div in containing div and display on same
    line.
    - apply general layout effects such as padding and margins.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -25,7 +25,7 @@ const signInSuccess = responseData => {
   // Hide auth forms that should only be visible when not signed in
   $('#auth-forms').fadeOut(500)
   // Show new items and index refresh buttons
-  $('#refresh-button, #new-inventory-item-button').show()
+  $('#refresh-button, #new-inventory-item-button, #storefront-table').show()
   // TODO
   // Index store's inventory and dispaly on screen
   // If inventory is empty, display a message (so it's clear there is no error)
@@ -43,6 +43,7 @@ const signOutSuccess = () => {
   store.user = null
   $('.menu-button').fadeOut(500)
   $('#sign-up-form, #refresh-button, #new-inventory-item-button, #storefront-table').hide()
+  $('#storefront-table').text('')
   $('#sign-in-form').show()
   $('#auth-forms').fadeIn(500)
 }
